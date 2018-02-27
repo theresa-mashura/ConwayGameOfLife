1e489f8d93b1f08cb274e0fb3c5a7e1748b2e986
xt0fer <kryounger@gmail.com>
2018-02-27 09:49:48 -0500

    initial commit

---
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -0,0 +1,43 @@
+package com.zipcodeconway;
+
+public class ConwayGameOfLife {
+
+    public ConwayGameOfLife(Integer dimension) {
+     }
+
+    public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+    }
+
+    public static void main(String[] args) {
+        ConwayGameOfLife sim = new ConwayGameOfLife(50);
+        int[][] endingWorld = sim.simulate(50);
+    }
+
+    // Contains the logic for the starting scenario.
+    // Which cells are alive or dead in generation 0.
+    // allocates and returns the starting matrix of size 'dimension'
+    private int[][] createRandomStart(Integer dimension) {
+        return new int[1][1];
+    }
+
+    public int[][] simulate(Integer maxGenerations) {
+        return new int[1][1];
+    }
+
+    // copy the values of 'next' matrix to 'current' matrix,
+    // and then zero out the contents of 'next' matrix
+    public void copyAndZeroOut(int [][] next, int[][] current) {
+    }
+
+    // Calculate if an individual cell should be alive in the next generation.
+    // Based on the game logic:
+	/*
+		Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
+		Any live cell with more than three live neighbours dies, as if by overcrowding.
+		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
+		Any dead cell with exactly three live neighbours cells will come to life.
+	*/
+    private int isAlive(int row, int col, int[][] world) {
+        return 0;
+    }
+}
